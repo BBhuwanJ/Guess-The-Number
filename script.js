9071e7bcf76637d024b71a27f23889e8f0810162
BBhuwanJ <mrstrange564@gmail.com>
2025-04-16 18:32:03 +0545

    write validate function

--- script.js
+++ script.js
@@ -10,9 +10,39 @@ const p = document.createElement('p')
 let prevGuess = []
 let numGuess = 1
 let playGame = true
-
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        console.log(guess);
+        
+        validateGuess(guess)
+    }) 
+}
 function validateGuess(guess){
-    
+    if(isNaN(guess)){
+        alert("Please Enter a valid number.")
+    }
+    else if(guess<1){
+        alert("Please Enter a number more than 1.")
+    }
+    else if(guess>100){
+        alert("Please Enter a number less than 100.")
+    }else{
+        preGuess.push(guess)
+        if(numGuess === 11){
+            displayGuess(guess)
+            displayMessage(`Game Over. Random number was ${randNum}`)
+            endGame()
+        }
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+            
+        }
+    }
+
+ 
 }
 
 function checkGuess(guess){
@@ -26,9 +56,10 @@ function displayMessage(message){
 
 }
 
-function newGame(){
+function endGame(){
     
 }
-function endGame(){
+
+function newGame(){
     
 }
