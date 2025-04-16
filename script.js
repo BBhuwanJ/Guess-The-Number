1dabbe59b9e83f420052a53c2527505d1446917b
BBhuwanJ <mrstrange564@gmail.com>
2025-04-16 18:23:48 +0545

    create necessary functions

---
+++ script.js
@@ -0,0 +1,34 @@
+const randNum = Math.round(Math.random()*100 +1)
+const submit = document.querySelector("#submit")
+const userInput = document.querySelector("#guessField")
+const preGuess = document.querySelector(".guesses")
+const lastRes = document.querySelector(".lastResult")
+const decision = document.querySelector(".lowOrHi")
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+let playGame = true
+
+function validateGuess(guess){
+    
+}
+
+function checkGuess(guess){
+
+}
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+function newGame(){
+    
+}
+function endGame(){
+    
+}
